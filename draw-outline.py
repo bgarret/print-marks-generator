a479e1394f2921ca8eb1356b7b629a7bb327a21a
Benoit Garret <benoit.garret@wyvernplatform.com>
2011-04-06 16:17:25 +0200

    Initial commit

---
+++ draw-outline.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import os.path
+from pyPdf import PdfFileWriter, PdfFileReader
+from outline import OutlineCreator, MM_TO_PT
+
+# Option parsing
+parser = OptionParser("Usage: %prog [options] filename")
+parser.add_option("-c", "--crop-margin", type="float", dest="crop_margin", default=15.)
+parser.add_option("-b", "--bleed-margin", type="float", dest="bleed_margin", default=5.)
+parser.add_option("-o", "--output", type="string", dest="output_filename", default="outline.pdf")
+
+(options, args) = parser.parse_args()
+
+if len(args) != 1 or not os.path.isfile(args[0]):
+  parser.print_help()
+  exit()
+
+# Read the input and prepare the output document
+filename = args[0]
+document = PdfFileReader(file(filename, "rb"))
+output = PdfFileWriter()
+
+for page_num in range(document.getNumPages()):
+  # Get the page dimensions
+  page = document.getPage(page_num)
+  box = page.mediaBox
+  # PDF dimensions are in points
+  width = round(float(box[2]) / MM_TO_PT)
+  height = round(float(box[3]) / MM_TO_PT)
+
+  # Create the outline
+  outline_creator = OutlineCreator(width, height, bleed=options.bleed_margin, crop=options.crop_margin)
+  outline = outline_creator.create()
+
+  # Merge the outline with the current page and add it to the output
+  output.addPage(PdfFileReader(outline).getPage(0))
+  output.getPage(page_num).mergeTranslatedPage(page, outline_creator.print_marks * MM_TO_PT, outline_creator.print_marks * MM_TO_PT)
+
+
+outputStream = file(options.output_filename, "wb")
+output.write(outputStream)
+outputStream.close()
---
+++ outline.py
@@ -0,0 +1,81 @@
+import cairo
+import math
+import StringIO
+
+MM_TO_PT = 2.834645669
+
+class OutlineCreator:
+
+  def __init__(self, width, height, bleed=5., crop=15.):
+    self.bleed, self.crop, self.width, self.height = bleed, crop, width, height
+    self.print_marks = crop - bleed
+    self.total_width = width + self.print_marks * 2
+    self.total_height = height + self.print_marks * 2
+
+    self.output = StringIO.StringIO()
+    self.surface = cairo.PDFSurface(self.output, self.total_width * MM_TO_PT, self.total_height * MM_TO_PT)
+
+  def create_context(self):
+    context = cairo.Context(self.surface)
+    context.set_source_rgb(0, 0, 0)
+    context.set_line_width(0.2)
+
+    context.scale(MM_TO_PT, MM_TO_PT)
+
+    return context
+
+  def put_circle(self, context, x, y, radius):
+    context.arc(x, y, radius, 0, math.pi * 2)
+    context.stroke()
+
+  def print_mark(self, x, y, margin):
+    context = self.create_context()
+    context.translate(x, y)
+
+    self.put_circle(context, 0, 0, 1./3. * margin)
+    self.put_circle(context, 0, 0, 1./6. * margin)
+
+    context.move_to(0, -3./8. * margin)
+    context.line_to(0, 3./8. * margin)
+
+    context.move_to(-3./8. * margin, 0)
+    context.line_to(3./8. * margin, 0)
+
+    context.stroke()
+
+  def print_line(self, x1, y1, x2, y2):
+    context = self.create_context()
+    context.move_to(x1, y1)
+    context.line_to(x2, y2)
+    context.stroke()
+
+  def crop_width(self):
+    return self.crop * 3. / 4.
+
+  def create(self):
+    # Top
+    self.print_mark(self.total_width / 2, self.print_marks / 2, self.print_marks)
+    # Bottom
+    self.print_mark(self.total_width / 2, self.total_height - self.print_marks / 2, self.print_marks)
+    # Left
+    self.print_mark(self.print_marks / 2, self.total_height / 2, self.print_marks)
+    # Right
+    self.print_mark(self.total_width - self.print_marks/ 2, self.total_height / 2, self.print_marks)
+
+    # Top cut
+    self.print_line(0, self.crop, self.crop_width(), self.crop)
+    self.print_line(self.total_width, self.crop, self.total_width - self.crop_width(), self.crop)
+    # Bottom cut
+    self.print_line(0, self.total_height - self.crop, self.crop_width(), self.total_height - self.crop)
+    self.print_line(self.total_width, self.total_height - self.crop, self.total_width - self.crop_width(), self.total_height - self.crop)
+    # Left cut
+    self.print_line(self.crop, 0, self.crop, self.crop_width())
+    self.print_line(self.crop, self.total_height, self.crop, self.total_height - self.crop_width())
+    # Right cut
+    self.print_line(self.total_width - self.crop, 0, self.total_width - self.crop, self.crop_width())
+    self.print_line(self.total_width - self.crop, self.total_height, self.total_width - self.crop, self.total_height - self.crop_width())
+
+    self.surface.flush()
+    self.surface.finish()
+
+    return self.output
